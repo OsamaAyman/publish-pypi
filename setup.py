04bc752e0008e4ab78f5dc08932d403adc925bc7
Osama Ayman <osama.ayman252@gmail.com>
2022-04-08 22:04:25 +0200

    add license and readme

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setup(name='distru',
       license='MIT',
       author="Osama Ayman",
       author_email='osama.ayman252@gmail.com',
-      url='',
+      url='https://github.com/OsamaAyman/publish-pypi',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
       install_requires=[
           'matplotlib',
-            'numpy'],
+          'numpy'],
       )
