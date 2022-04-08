ecb816280e1a8b79b4d4e04ff1b89f9f666e6a10
Osama Ayman <osama.ayman252@gmail.com>
2022-04-08 17:22:24 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='distru',
+      version='0.1',
+      description="Package to generate or read different distributions.\
+                  Visualize and make some operations on the distributions",
+      license='MIT',
+      author="Osama Ayman",
+      author_email='osama.ayman252@gmail.com',
+      url='',
+      install_requires=[
+          'matplotlib',
+            'numpy'],
+      )
